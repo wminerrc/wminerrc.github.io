f8d996b0eb630e9cd428f8b444bb8de57121afd7
Leonardo Lana <leonardo.lana@outlook.com.br>
2024-02-22 14:26:46 -0300

    uh what an ugly miner

--- src/script.js
+++ src/script.js
@@ -249,7 +249,7 @@ const template = `
         <span class="fechar" onclick="document.getElementById('balao').style.display = 'none';">X</span>
         <p>{{bestDiscount}}% de desconto! Preencha o nosso form.</p>
         <p>Ajude o canal e ainda participe de sorteios insanos!</p>
-        <a href="https://forms.gle/36n7mE6uTmDBMk7S9" target="_blank">Acessar o form</a>
+        <a href="https://forms.gle/8yw3K9ajFP2TZ5ak6" target="_blank">Acessar o form</a>
         
     </div>
 `
