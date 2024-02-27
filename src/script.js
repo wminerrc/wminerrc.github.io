01e79d8fd729be3b05c77d917fa2896970d11200
Leonardo Lana <leonardo.lana@outlook.com.br>
2024-02-27 12:02:05 -0300

    new evt

--- src/script.js
+++ src/script.js
@@ -249,7 +249,7 @@ const template = `
         <span class="fechar" onclick="document.getElementById('balao').style.display = 'none';">X</span>
         <p>{{bestDiscount}}% de desconto! Preencha o nosso form.</p>
         <p>Ajude o canal e ainda participe de sorteios insanos!</p>
-        <a href="https://forms.gle/8yw3K9ajFP2TZ5ak6" target="_blank">Acessar o form</a>
+        <a href="https://forms.gle/YqNtL5mZRLSLAj7n7" target="_blank">Acessar o form</a>
         
     </div>
 `
