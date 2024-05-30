139d34b32bbe579cc114320ac790071d989ddd95
Leonardo Lana <leonardo.lana@outlook.com.br>
2024-05-30 16:53:36 -0300

    sad

---
+++ calculator/app.js
@@ -0,0 +1,107 @@
+angular.module('miningApp', [])
+.controller('MiningController', ['$scope', 'CurrencyService', async function($scope, CurrencyService) {
+    $scope.units = ['GH/s', 'TH/s', 'PH/s', 'EH/s'];
+    $scope.networkUnits = ['GH/s', 'TH/s', 'PH/s', 'EH/s', 'ZH/s'];
+    let default_form = {
+        currency: null,
+        power: 0,
+        unit: $scope.units[0],
+        networkPower: 0,
+        networkUnit: $scope.networkUnits[0],
+        blockSize: 0,
+        blockTime: 0,
+        timeUnit: 'seconds'
+    };
+    $scope.formData = default_form;
+    $scope.isLoading = true;
+    const exchangeRates = await CurrencyService.getCurrenciesPrices();
+    $scope.exchangeRates = exchangeRates;
+
+    const convertHashrate = (value, fromUnit, toUnit) => {
+        const units = {
+            'GH/s': 1,
+            'TH/s': 1000,
+            'PH/s': 1000000,
+            'EH/s': 1000000000,
+            'ZH/s': 1000000000000
+        };
+        return value * units[fromUnit] / units[toUnit];
+    };
+
+    const convertTime = (value, fromUnit, toUnit) => {
+  
+        if (fromUnit === 'minutes' && toUnit === 'seconds') {
+            return value * 60;
+        } else if (fromUnit === 'seconds' && toUnit === 'minutes') {
+            return value / 60;
+        } else {
+            return value;
+        }
+    };
+
+    $scope.currencies = await CurrencyService.getDetailedCurrencies();
+    $scope.isLoading = false;
+    $scope.$apply();
+
+    $scope.updateNetworkPowerUnit = function(oldUnit) {
+        $scope.formData.networkPower = convertHashrate($scope.formData.networkPower, oldUnit, $scope.formData.networkUnit);
+    };
+
+    $scope.updatePowerUnit = function(oldUnit) {
+        $scope.formData.power = convertHashrate($scope.formData.power, oldUnit, $scope.formData.unit);
+    };
+
+    $scope.updateTimeUnit = function(oldUnit) {
+        $scope.formData.blockTime = convertTime($scope.formData.blockTime, oldUnit, $scope.formData.timeUnit);
+    };
+
+    $scope.updateCurrencyDetails = function() {
+        const selectedCurrency = $scope.formData.currency;
+        if (selectedCurrency) {
+            $scope.formData.networkPower = selectedCurrency.networkPower;
+            $scope.formData.networkUnit = selectedCurrency.networkUnit;
+            $scope.formData.blockSize = selectedCurrency.blockSize;
+            $scope.formData.blockTime = selectedCurrency.blockTime;
+            $scope.formData.timeUnit = 'seconds';
+        }else {
+            $scope.formData.power = 0;
+            $scope.formData.unit = $scope.units[0];
+            $scope.formData.networkPower = 0;
+            $scope.formData.networkUnit = $scope.networkUnits[0];
+            $scope.formData.blockSize = 0;
+            $scope.formData.blockTime = 0;
+            $scope.formData.timeUnit = 'seconds';
+        }
+    };
+
+    $scope.calculateEarnings = function(timeframe, currency) {
+        if (!$scope.formData.currency || !$scope.formData.blockSize || !$scope.formData.blockTime) {
+            return 0;
+        }
+        
+        let earningsPerBlock = $scope.formData.blockSize;
+        let blockTimeInSeconds = $scope.formData.blockTime;
+
+        if ($scope.formData.timeUnit === 'minutes') {
+            blockTimeInSeconds *= 60;
+        }
+
+        let userPowerPercentage = convertHashrate($scope.formData.power, $scope.formData.unit, 'GH/s') / convertHashrate($scope.formData.networkPower, $scope.formData.networkUnit,'GH/s');
+        earningsPerBlock *= userPowerPercentage;
+
+        let earningsPerDay = earningsPerBlock * (86400 / blockTimeInSeconds); // 86400 seconds in a day
+        
+        switch(timeframe) {
+            case 'block':
+                return currency === 'amount' ? earningsPerBlock.toFixed(6) : $scope.formData.currency.in_game_only ? 0 : (earningsPerBlock * exchangeRates[$scope.formData.currency.name][currency]).toFixed(2);
+            case 'day':
+                return currency === 'amount' ? earningsPerDay.toFixed(6) : $scope.formData.currency.in_game_only ? 0 : (earningsPerDay * exchangeRates[$scope.formData.currency.name][currency]).toFixed(2);
+            case 'week':
+                return currency === 'amount' ? (earningsPerDay * 7).toFixed(6) : $scope.formData.currency.in_game_only ? 0 : (earningsPerDay * 7 * exchangeRates[$scope.formData.currency.name][currency]).toFixed(2);
+            case 'month':
+                return currency === 'amount' ? (earningsPerDay * 30).toFixed(6) : $scope.formData.currency.in_game_only ? 0 : (earningsPerDay * 30 * exchangeRates[$scope.formData.currency.name][currency]).toFixed(2);
+            default:
+                return 0;
+        }
+    };
+}]);
---
+++ calculator/services.js
@@ -0,0 +1,149 @@
+angular.module('miningApp')
+.service('CurrencyService', ['$http', '$q', function($http, $q) {
+
+    const current_date = new Date().toISOString().split('T')[0];
+
+    var delay = function(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    };
+
+    const cacheValidityDuration = 24 * 60 * 60 * 1000;
+
+    const getCache = (cacheKey) => {
+        const cacheTimestamp = localStorage.getItem(`${cacheKey}_exp`);
+        if (cacheTimestamp) {
+            const now = new Date().getTime();
+            const age = now - cacheTimestamp;
+            if (age < cacheValidityDuration) {
+                return JSON.parse(localStorage.getItem(cacheKey));
+            }
+        }
+        return null;
+    };
+
+    const setCache = (data, cacheKey) => {
+        localStorage.setItem(cacheKey, JSON.stringify(data));
+        localStorage.setItem(`${cacheKey}_exp`, new Date().getTime().toString());
+    };
+
+    
+    var getCurrenciesPrices = async function() {
+        const cache_key = 'exchange_history';
+        var cached = getCache(cache_key);
+        if(cached) return cached;
+        const currencies =
+            [
+                {
+                    name: 'MATIC', coingecko_id : 'matic-network'
+                },
+                {
+                    name: 'BNB', coingecko_id : 'binancecoin'
+                },
+                {
+                    name: 'LTC', coingecko_id : 'litecoin'
+                },
+                {
+                    name: 'SOL', coingecko_id : 'solana'
+                },
+                {
+                    name: 'ETH', coingecko_id : 'ethereum'
+                },
+                {
+                    name: 'TRX', coingecko_id : 'tron'
+                },
+                {
+                    name: 'BTC', coingecko_id : 'bitcoin'
+                },
+                {
+                    name: 'DOGE', coingecko_id : 'dogecoin'
+                }
+            ];
+
+        return $http.get(`https://api.coingecko.com/api/v3/simple/price?ids=${currencies.map(c => c.coingecko_id).join()}&vs_currencies=usd,brl`).then(response => {
+            if (response.status === 200) { 
+                var result = response.data;
+                currencies.forEach(c => {
+                    delete Object.assign(result, {[c.name]: result[c.coingecko_id] })[c.coingecko_id];
+                })
+                setCache(result, cache_key);
+                return result;
+            }
+        });
+    };
+
+    var getCurrencies = function() {
+        return $http.get(`https://rollercoin.free.mockoapp.net/get?url=${encodeURIComponent('https://rollercoin.com/api/wallet/get-currencies-config')}`).then(response => {
+            if (response.status === 200) { 
+                const result = JSON.parse(response.data.contents);
+                return result.data.currencies_config.filter(c => c.is_can_be_mined).map(c => ({
+                    name: c.name,
+                    in_game_only: c.network === '' || c.network === 'Rollertoken',
+                    balance_key: c.balance_key,
+                    to_small: c.to_small,
+                    divider: c.divider,
+                    image: `https://rollercoin.com/static/img/icon/currencies/${c.img}.svg?v=1.0`
+                }));
+            }
+        });
+    };
+
+    var getBlockSizeByCurrency = async function(currency) {
+        await delay(1500);
+        const search = `https://rollercoin.com/api/mining/network-info-by-day?from=${current_date}&to=${current_date}&currency=${currency}&groupBy=block_reward`;
+        return $http.get(`https://rollercoin.free.mockoapp.net/get?url=${encodeURIComponent(search)}`).then(response => {
+            if (response.status === 200) { 
+                const result = JSON.parse(response.data.contents);
+                return result.data[0].value;
+            }
+        });
+    };
+
+    var getNetworkPowerByCurrency = async function(currency) {
+        await delay(1500);
+        const search = `https://rollercoin.com/api/mining/network-info-by-day?from=${current_date}&to=${current_date}&currency=${currency}&groupBy=total_power`;
+        return $http.get(`https://rollercoin.free.mockoapp.net/get?url=${encodeURIComponent(search)}`).then(response => {
+            if (response.status === 200) { 
+                const result = JSON.parse(response.data.contents);
+                return result.data[0].value;
+            }
+        });
+    };
+
+    var getBlockTimeByCurrency = async function(currency) {
+        await delay(1500);
+        const search = `https://rollercoin.com/api/mining/network-info-by-day?from=${current_date}&to=${current_date}&currency=${currency}&groupBy=duration`;
+        return $http.get(`https://rollercoin.free.mockoapp.net/get?url=${encodeURIComponent(search)}`).then(response => {
+            if (response.status === 200) { 
+                const result = JSON.parse(response.data.contents);
+                return result.data[0].value;
+            }
+        });
+    };
+    
+    this.getCurrencies = getCurrencies;
+    this.getCurrenciesPrices = getCurrenciesPrices;
+
+    this.getDetailedCurrencies = async function() {
+        const cache_key = 'rc_network_data';
+        var cached = getCache(cache_key);
+        if(cached) return cached;
+        const currencies = await getCurrencies();
+        const detailedCurrencies = [];
+        for (const currency of currencies) {
+            const [blockSize, networkPower, blockTime] = await Promise.all([
+                getBlockSizeByCurrency(currency.balance_key),
+                getNetworkPowerByCurrency(currency.balance_key),
+                getBlockTimeByCurrency(currency.balance_key)
+            ]);
+            currency.values = [blockSize, networkPower, blockTime];
+            currency.blockSize = (blockSize/currency.divider) / currency.to_small;
+            currency.networkPower = networkPower;
+            currency.blockTime = blockTime;
+            currency.networkUnit = 'GH/s';
+            detailedCurrencies.push(currency);
+        }
+        setCache(detailedCurrencies, cache_key);
+        return detailedCurrencies;
+    };
+
+}]);
