96dfb1699b34b5e8a0727277d7f7769789321f5f
Leonardo Lana <leonardo.lana@outlook.com.br>
2024-07-27 01:04:35 -0300

    donation

--- calculator/app.js
+++ calculator/app.js
@@ -63,7 +63,7 @@ app.controller('MiningController', ['$scope', 'CurrencyService', 'UserMinerServi
             const currency = $scope.donationCurrency === 'U$' ? 'usd' : 'brl';
             $scope.donationInBnb = ($scope.donationValue / exchangeRates['BNB'][currency]).toFixed(3)
             $scope.donationInMatic = ($scope.donationValue / exchangeRates['MATIC'][currency]).toFixed(2)
-            $scope.donationInEth = $scope.donationValue / exchangeRates['ETH'][currency].toFixed(18)
+            $scope.donationInEth = ($scope.donationValue / exchangeRates['ETH'][currency]).toFixed(18)
         }
     }
 
