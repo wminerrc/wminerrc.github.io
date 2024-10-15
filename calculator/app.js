a01af5f8f278eec406cc1e776f13cc24ebabd121
Leonardo Lana <leonardo.lana@outlook.com.br>
2024-10-14 23:24:45 -0300

    recipe

--- calculator/app.js
+++ calculator/app.js
@@ -427,6 +427,17 @@ app.controller('MiningController', ['$scope', 'CurrencyService', 'UserMinerServi
         window.open(`https://rollercoin.com/marketplace/buy/miner/${item.miner_id}`,'_blank');
     }
 
+    $scope.openBuyCraftLink = async function(id, type) {
+        if(!localStorage.getItem('alreadyDonatedMessage')) {
+            localStorage.setItem('alreadyDonatedMessage', 'true');
+            if(confirm('Te ajudei a tomar essa decisão de compra? Considere fazer uma contribuição para manter o desenvolvimento desse projeto')) {
+                window.scrollTo(0, document.body.scrollHeight);
+                return;
+            }
+        }
+        window.open(`https://rollercoin.com/marketplace/buy/${type}/${id}`,'_blank');
+    }
+
     $scope.addMinerToSimulation = async function($item) {
         $scope.customMiners = $scope.customMiners || [];
         $scope.customMiners.push({...$item, rdid: uuidv4()});
