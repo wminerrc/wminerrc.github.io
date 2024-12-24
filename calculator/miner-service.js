50858ac63412838837f40868b3908500b3e7f061
Leonardo Lana <leonardo.lana@outlook.com.br>
2024-12-24 12:13:39 -0300

    evt

--- calculator/miner-service.js
+++ calculator/miner-service.js
@@ -2,11 +2,20 @@ let servicez = angular.module('miningApp');
 servicez.service('MinerService', ['$http', '$q', function($http, $q) {
     const basic_miners = window.basic_miners;
     const merge_miners = window.merge_miners;
-    basic_miners.forEach(m => m.craft_cost_diff = m.craft_cost_diff || 0);
+    const old_merge_miners = window.old_merge_miners;
+    merge_miners.forEach(m => m.old_craft_items =  old_merge_miners.find(om => om.miner_id === m.miner_id)?.craft_items )
     basic_miners.forEach(m => m.recipe_cost = 0);
-    merge_miners.forEach(m => m.craft_cost_diff = m.craft_cost_diff || 0);
+    basic_miners.forEach(m => m.old_recipe_cost = 0);
     merge_miners.forEach(m => m.recipe_cost = m.craft_items.filter(i => i.component_type === 'mutation_components').map(r => window.components.find(c => c.id === r._id).avg_price * r.count ));
     merge_miners.forEach(m => m.recipe_cost = parseFloat((m.recipe_cost / 1000000).toFixed(2)));
+    merge_miners.forEach(m => m.old_recipe_cost = m.old_craft_items?.filter(i => i.component_type === 'mutation_components').map(r => window.components.find(c => c.id === r._id).avg_price * r.count ));
+    merge_miners.forEach(m => m.old_recipe_cost = parseFloat((m.old_recipe_cost / 1000000).toFixed(2)));
+    merge_miners.forEach(m => m.recipe_cost = m.recipe_cost || 0);
+    merge_miners.forEach(m => m.old_recipe_cost = m.old_recipe_cost || 0);
+    const calculateDiscount = (oldPrice, currentPrice) => ((oldPrice - currentPrice) / oldPrice * 100).toFixed(2);
+    merge_miners.forEach(m => m.craft_cost_diff = calculateDiscount(m.old_recipe_cost, m.recipe_cost))
+    merge_miners.forEach(m => m.craft_cost_diff = isNaN(m.craft_cost_diff) ? 0 : m.craft_cost_diff);
+    basic_miners.forEach(m => m.craft_cost_diff = 0)
     this.getMinersByName = async function(name) {
         return JSON.parse(JSON.stringify(basic_miners)).filter(m => m.name.en.toLowerCase().includes(name.toLowerCase()));
     };
